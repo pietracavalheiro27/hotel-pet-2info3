82b56c1ce5c0d90421ef097cf16a53db3992e923
pietracavalheiro27 <cavalheirojpietra@gmail.com>
2026-09-25 15:15:16 -0300

    alteracao

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/HomeView.vue'),
+      component: () => import('../views/PetsView.vue'),
+    },
+    {
+      path: '/pets/novo',
+      name: 'addPet',
+      component: () => import('../views/AddPetView.vue/index.js'),
     },
   ],
 });
---
+++ src/views/PetsViews.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <header class="mb-4">
+      <h1 class="text-2xl font-bold">Listagem de Pets</h1>
+      <p class="text-body-secondary mb-0">
+        Listagem dos Pets cadastrados no sistema.
+      </p>
+    </header>
+
+    <RouterLink
+      class="btn btn-primary"
+      :to="{ name: 'addPet' }"
+    >
+      Adicionar Pet
+    </RouterLink>
+  </div>
+</template>
